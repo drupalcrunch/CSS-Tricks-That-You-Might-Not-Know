7dff667df77dbb226d60772b4023ca40261ab351
Mike Herchel <mike@herchel.com>
2014-11-05 21:46:07 -0500

    Some basic code highlighting.

--- js/herchel.js
+++ js/herchel.js
@@ -1,3 +1,13 @@
 $(document ).ready(function() {
+  $('code').click(function() {
+    $(this).addClass('language-none');
+    $('span', this).contents().unwrap();
+    $(this).wrapInner('<style contenteditable class="editable-style" type="text/css"></style>');
+    $(this).contents().unwrap();
+  });
 
+  $('style[contenteditable]').blur(function() {
+    $(this).wrapInner('<code class="language-css"></code>');
+    $(this).contents().unwrap();
+  })
 });
